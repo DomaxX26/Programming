a3111fa1bc659a91dfae55ea9d04dc18f07a6ac4
Joan Pons <joan.ponssanchis@gmail.com>
2021-12-10 13:07:15 +0100

    New Commit

--- PHP/movieFX-2122-main/Dni.php
+++ PHP/movieFX-2122-main/Dni.php
@@ -5,8 +5,20 @@ namespace App;
 use LengthException;
 class Dni
 {
-    public function __construct()
+    private const VALID_LENGHT = 9;
+    public function __construct(string $dni)
     {
-        throw new LengthException("To Long");
+        $this->checkDniHasValidLenght($dni);
+
+        if(preg_match("/\d$/", $dni)){
+            throw new \DomainException("Ends with a number");
+        }
+        throw new \DomainException("Ends with an invalid letter");
+    }
+
+    private function checkDniHasValidlength(string $dni):void{
+        if (strlen($dni) !== self::VALID_LENGHT){
+            throw new LengthException("Too long or too shoort");
+        }
     }
 }
