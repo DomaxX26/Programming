d1d43fd793ff40b752b28bc94c186cc32a012f7d
Joan Pons <joan.ponssanchis@gmail.com>
2021-12-12 20:02:07 +0100

    JS

--- JavaScript/2º DAW/Tema 8/Carregar les poblacions/index.js
+++ JavaScript/2º DAW/Tema 8/Carregar les poblacions/index.js
@@ -5,6 +5,43 @@ function main(){
 }
 
 function carregarPoblacions(){
-    poblacionsAlacant = new XMLHttpRequest();
+    poblacionsCastello = new XMLHttpRequest();
     poblacionsValencia = new XMLHttpRequest();
+    poblacionsAlacant = new XMLHttpRequest();
+
+    poblacionsCastello.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+            let objecte = JSON.parse(this.responseText);
+            objecte.forEach(element => {
+                console.log(element.DMUN50);
+            });
+        }
+    }
+    poblacionsCastello.open("GET", "https://apiv1.geoapi.es/municipios?CPRO=12&type=JSON&key=&sandbox=1", true);
+    poblacionsCastello.send();
+
+    poblacionsValencia.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+            let objecte = JSON.parse(this.responseText);
+            objecte.forEach(element => {
+                console.log(element);
+            });
+        }
+    }
+    poblacionsValencia.open("GET", "https://apiv1.geoapi.es/municipios?CPRO=46&type=JSON&key=&sandbox=1", true);
+    poblacionsValencia.send();
+
+    poblacionsAlacant.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+            let objecte = JSON.parse(this.responseText);
+            objecte.forEach(element => {
+                console.log(element.DMUN50);
+            });
+        }
+    }
+    poblacionsAlacant.open("GET", "https://apiv1.geoapi.es/municipios?CPRO=03&type=JSON&key=&sandbox=1", true);
+    poblacionsAlacant.send();
 }
