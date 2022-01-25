7f03e3c83639b5dd21c147d85a1e66436ae12684
Joan Pons <joan.ponssanchis@gmail.com>
2022-01-25 01:17:11 +0100

    JS

--- JavaScript/2º DAW/Tema 8/AlumnesRestauranteParte2/AlumnesRestaurante_Part2/js/comanda.js
+++ JavaScript/2º DAW/Tema 8/AlumnesRestauranteParte2/AlumnesRestaurante_Part2/js/comanda.js
@@ -1,13 +1,19 @@
 window.onload = main;
 
-function main(){
-	if (JSON.parse(localStorage.getItem("token") != null)) {
-		
-	}
+function main() {
+    if (JSON.parse(localStorage.getItem("token") != null)) {
+        getDatos();
+    }
 }
 
-function carregarTaules(){
-	fetch("https://restaurante.serverred.es/api/mesas", {
+async function getDatos() {
+    await carregarTaules();
+    await carregarCamareros();
+    await carregarComandas();
+}
+
+function carregarTaules() {
+    fetch("https://restaurante.serverred.es/api/mesas", {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -17,6 +23,7 @@ function carregarTaules(){
         .then(response => response.json())
         .then(data => {
             data.data.data.forEach(element => {
+                console.log(element);
                 mostrarMesas(element);
             });
         })
@@ -25,8 +32,8 @@ function carregarTaules(){
         })
 }
 
-function carregarBegudes(){
-	fetch("https://restaurante.serverred.es/api/bebidas", {
+function carregarCamareros() {
+    fetch("https://restaurante.serverred.es/api/camareros", {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -36,7 +43,8 @@ function carregarBegudes(){
         .then(response => response.json())
         .then(data => {
             data.data.data.forEach(element => {
-                mostrarBebidas(element);
+                //console.log(element);
+                //mostrarCamareros(element);
             });
         })
         .catch((error) => {
@@ -44,21 +52,72 @@ function carregarBegudes(){
         })
 }
 
-function carregarPlats(){
-	fetch("https://restaurante.serverred.es/api/platos", {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"auth-token": localStorage.getItem("token")
-		}
-	})
-		.then(response => response.json())
-		.then(data => {
-			data.data.data.forEach(element => {
-				mostrarPlatos(element);
-			});
-		})
-		.catch((error) => {
-			console.log("Error => ", error);
-		})
+function carregarComandas() {
+    fetch("https://restaurante.serverred.es/api/comandas", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "auth-token": localStorage.getItem("token")
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            data.data.data.forEach(element => {
+                console.log(element);
+                //mostrarComandas(element);
+            });
+        })
+        .catch((error) => {
+            console.log("Error => ", error);
+        })
+}
+
+function mostrarMesas(element){
+    let files = document.getElementById("files");
+
+    let tr = document.createElement("tr");
+
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let td5 = document.createElement("td");
+    let td6 = document.createElement("td");
+    let td7 = document.createElement("td");
+
+    //Botones para editar la comanda de bebidas
+    let sumaBebida = document.createElement("i");
+    sumaBebida.setAttribute("class", "fas fa-plus");
+
+    let editarBebidas = document.createElement("button");
+    editarBebidas.setAttribute("class", "btn btn-info btn-lg p-2");
+
+    let textEditarBebidas = document.createTextNode("Bebidas");
+    editarBebidas.appendChild(sumaBebida);
+    editarBebidas.appendChild(textEditarBebidas);
+
+    //Botón para editar la comanda de platos
+    let sumaPlatos = document.createElement("i");
+    sumaPlatos.setAttribute("class", "fas fa-plus");
+
+    let editarPlatos = document.createElement("button");
+    editarPlatos.setAttribute("class", "btn btn-info btn-lg p-2");
+
+    let texteditarPlatos = document.createTextNode("Platos");
+    editarPlatos.appendChild(sumaPlatos);
+    editarPlatos.appendChild(texteditarPlatos);
+    
+    let mesa = document.createTextNode(element.numero);
+    let comensales = document.createTextNode(element.comensales);
+
+    td1.appendChild(editarBebidas);
+    td2.appendChild(editarPlatos);
+    td4.appendChild(mesa);
+
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td4);
+
+    files.appendChild(tr);
 }
