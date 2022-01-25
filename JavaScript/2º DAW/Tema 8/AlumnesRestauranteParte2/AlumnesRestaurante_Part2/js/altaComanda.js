74585b69d10359355c9c8240f131b28c4d2f7e22
Joan Pons <joan.ponssanchis@gmail.com>
2022-01-25 21:43:21 +0100

    JS

---
+++ JavaScript/2º DAW/Tema 8/AlumnesRestauranteParte2/AlumnesRestaurante_Part2/js/altaComanda.js
@@ -0,0 +1,234 @@
+window.onload = main;
+var infoTaula = [];
+
+function main() {
+	if (JSON.parse(localStorage.getItem("token") != null)) {
+		getDatos();
+		document.getElementById("confirmar").addEventListener("click", validar, false);
+	} else {
+		location.assign("login.html");
+	}
+}
+
+async function getDatos() {
+	await carregarPlatos();
+	await carregarBebidas();
+	await carregarTaules();
+}
+
+async function carregarTaules() {
+	fetch("https://restaurante.serverred.es/api/mesas", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"auth-token": localStorage.getItem("token")
+		}
+	})
+		.then(response => response.json())
+		.then(data => {
+			data.data.data.forEach(element => {
+				crearMesas(element);
+				infoTaula.push(element);
+			});
+		})
+		.catch((error) => {
+			console.log("Error => ", error);
+		})
+}
+
+async function carregarBebidas() {
+	fetch("https://restaurante.serverred.es/api/bebidas", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"auth-token": localStorage.getItem("token")
+		}
+	})
+		.then(response => response.json())
+		.then(data => {
+			data.data.data.forEach(element => {
+				crearBebidas(element);
+			});
+		})
+		.catch((error) => {
+			console.log("Error => ", error);
+		})
+}
+
+async function carregarPlatos() {
+	fetch("https://restaurante.serverred.es/api/platos", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"auth-token": localStorage.getItem("token")
+		}
+	})
+		.then(response => response.json())
+		.then(data => {
+			data.data.data.forEach(element => {
+				crearPlatos(element);
+			});
+		})
+		.catch((error) => {
+			console.log("Error => ", error);
+		})
+}
+
+function crearMesas(element) {
+	let mesas = document.getElementById("mesas");
+
+	let div = document.createElement("div");
+	div.setAttribute("class", "col");
+
+	let input = document.createElement("input");
+	input.setAttribute("type", "button");
+	input.setAttribute("id", element._id);
+	input.setAttribute("class", "mt-2 btn btn-primary p-3");
+	input.setAttribute("value", element.numero);
+	input.addEventListener("click", cambiarColor);
+
+	div.appendChild(input);
+
+	mesas.appendChild(div);
+}
+
+function cambiarColor(){
+	infoTaula.forEach(element => {
+        document.getElementById(element._id).setAttribute("class", "mt-2 btn btn-primary p-3")
+    })
+
+    var infoMesa = infoTaula.find(item => item._id == this.id);
+	var button = document.getElementById(infoMesa._id);
+	button.setAttribute("class", "mt-2 btn btn-danger p-3");
+	
+	let datosMesa = document.getElementById("datosMesa");
+	let txtMesa = document.createTextNode("Numero de comensales: " + infoMesa.comensales+ ", Descripción: " + infoMesa.descripcion);
+	datosMesa.replaceChildren(txtMesa);
+}
+
+
+
+function crearBebidas(element) {
+	let bebidas = document.getElementById("bebidas");
+
+	let div = document.createElement("div");
+	div.setAttribute("class", "col");
+
+	let input = document.createElement("input");
+	input.setAttribute("type", "button");
+	input.setAttribute("id", element._id);
+	input.setAttribute("class", "mt-2 btn btn-info p-3");
+	input.setAttribute("value", element.nombre);
+
+	div.appendChild(input);
+
+	bebidas.appendChild(div);
+}
+
+function crearPlatos(element) {
+	let div = document.createElement("div");
+	div.setAttribute("class", "col");
+
+	let input = document.createElement("input");
+	input.setAttribute("type", "button");
+	input.setAttribute("id", element._id);
+	input.setAttribute("class", "mt-2 btn btn-warning p-3");
+	input.setAttribute("value", element.nombre);
+
+	div.appendChild(input);
+	document.getElementById("platos" + element.orden).appendChild(div);
+}
+
+
+function validarNombre() {
+	let nom = document.getElementById("nombre");
+
+	if (!nom.checkValidity()) {
+		if (nom.validity.valueMissing) {
+			error2(nom, "Nom obligatori");
+			return false;
+		}
+		if (nom.validity.patternMismatch) {
+			error2(nom, "El nom no esta dins del rang (4 - 60) caràcters");
+			return false;
+		}
+	}
+	return true;
+}
+
+function validarComensales() {
+    let comensales = document.getElementById("comensales");
+
+    if (!comensales.checkValidity()) {
+        if (comensales.validity.valueMissing) {
+            error2(comensales, "Error: Numero de comensales obligatorio");
+            return false;
+        }
+        if (comensales.validity.rangeOverflow) {
+            error2(comensales, "Error: Numero de comensales por encima de lo permitido");
+            return false;
+        }
+        if (comensales.validity.rangeUnderflow) {
+            error2(comensales, "Error: Numero de comensales inferior de lo permitido");
+            return false;
+        }
+    }
+    return true;
+}
+
+function nuevaComanda(){
+	console.log(infoTaula);
+	let nombre = document.getElementById("nombre").value;
+	let comensales = document.getElementById("comensales").value;
+	let notas = document.getElementById("notas").value;
+
+	var comanda = {
+		nom : nombre,
+		comensals: comensales,
+		estado: "Pendiete",
+		mesa: infoTaula._id,
+		bebidas: [],
+		platos:[],
+		notas: notas
+	}
+
+	fetch("https://restaurante.serverred.es/api/comandas", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"auth-token": localStorage.getItem("token")
+		},
+		body: JSON.stringify(comanda)
+	})
+		.then(response => response.json())
+		.then(data => {
+			
+		})
+		.catch((error) => {
+			console.log("Error => ", error);
+		})
+}
+
+function esborrarError() {
+	let formulari = document.forms[0];
+	for (let i = 0; i < formulari.elements.length - formulari.elements.length; i++) {
+		formulari.elements[i].className = "form-control";
+	}
+	document.getElementById("missatgeError").innerHTML = "";
+}
+
+function error2(element, missatge) {
+	document.getElementById("missatgeError").innerHTML = missatge;
+	element.className = "error form-control";
+}
+
+function validar(e) {
+	esborrarError();
+	e.preventDefault();
+	if (validarNombre() && validarComensales()) {
+		nuevaComanda();
+		return true;
+	} else {
+		return false;
+	}
+}
--- JavaScript/2º DAW/Tema 8/AlumnesRestauranteParte2/AlumnesRestaurante_Part2/js/comanda.js
+++ JavaScript/2º DAW/Tema 8/AlumnesRestauranteParte2/AlumnesRestaurante_Part2/js/comanda.js
@@ -1,18 +1,25 @@
 window.onload = main;
+var infoCamareros = [];
+var infoComandas = [];
 
 function main() {
     if (JSON.parse(localStorage.getItem("token") != null)) {
         getDatos();
+        document.getElementById("newComanda").addEventListener("click", function(){
+            location.assign("altaComandas.html");
+        })
+    }else {
+        location.assign("login.html");
     }
 }
 
 async function getDatos() {
-    await carregarTaules();
-    await carregarCamareros();
     await carregarComandas();
+    await carregarCamareros();
+    await carregarTaules();
 }
 
-function carregarTaules() {
+async function carregarTaules() {
     fetch("https://restaurante.serverred.es/api/mesas", {
         method: "GET",
         headers: {
@@ -23,8 +30,7 @@ function carregarTaules() {
         .then(response => response.json())
         .then(data => {
             data.data.data.forEach(element => {
-                console.log(element);
-                mostrarMesas(element);
+                mostrarTabla(element);
             });
         })
         .catch((error) => {
@@ -32,7 +38,7 @@ function carregarTaules() {
         })
 }
 
-function carregarCamareros() {
+async function carregarCamareros() {
     fetch("https://restaurante.serverred.es/api/camareros", {
         method: "GET",
         headers: {
@@ -42,17 +48,14 @@ function carregarCamareros() {
     })
         .then(response => response.json())
         .then(data => {
-            data.data.data.forEach(element => {
-                //console.log(element);
-                //mostrarCamareros(element);
-            });
+            infoCamareros.push(data.data.data);
         })
         .catch((error) => {
             console.log("Error => ", error);
         })
 }
 
-function carregarComandas() {
+async function carregarComandas() {
     fetch("https://restaurante.serverred.es/api/comandas", {
         method: "GET",
         headers: {
@@ -62,17 +65,16 @@ function carregarComandas() {
     })
         .then(response => response.json())
         .then(data => {
-            data.data.data.forEach(element => {
-                console.log(element);
-                //mostrarComandas(element);
-            });
+            console.log(data.data.data)
+            infoComandas.push(data.data.data);
         })
         .catch((error) => {
             console.log("Error => ", error);
         })
 }
 
-function mostrarMesas(element){
+var cont = 0;
+function mostrarTabla(element){
     let files = document.getElementById("files");
 
     let tr = document.createElement("tr");
@@ -101,23 +103,37 @@ function mostrarMesas(element){
     sumaPlatos.setAttribute("class", "fas fa-plus");
 
     let editarPlatos = document.createElement("button");
-    editarPlatos.setAttribute("class", "btn btn-info btn-lg p-2");
+    editarPlatos.setAttribute("class", "btn btn-warning btn-lg p-2");
 
     let texteditarPlatos = document.createTextNode("Platos");
     editarPlatos.appendChild(sumaPlatos);
     editarPlatos.appendChild(texteditarPlatos);
     
+    let nombre = document.createTextNode(infoComandas[0][cont].nombre);
     let mesa = document.createTextNode(element.numero);
     let comensales = document.createTextNode(element.comensales);
+    let camarero = document.createTextNode(infoCamareros[0][0].name);
+    let horaEntrada = document.createTextNode(infoComandas[0][cont].fechaEntrada.slice(11,-8));
+    cont++;
+    
 
     td1.appendChild(editarBebidas);
     td2.appendChild(editarPlatos);
+    td3.appendChild(nombre);
     td4.appendChild(mesa);
+    td5.appendChild(comensales);
+    td6.appendChild(camarero);
+    td7.appendChild(horaEntrada);
+
 
 
     tr.appendChild(td1);
     tr.appendChild(td2);
+    tr.appendChild(td3);
     tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+    tr.appendChild(td7);
 
     files.appendChild(tr);
 }
